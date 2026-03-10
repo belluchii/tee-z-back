166854124a7fe25db9522e390c1e9536144e66c2
valentin <bellberger45@gmail.com>
2026-03-10 18:11:29 -0300

    hotfix services

--- src/services/productService.js
+++ src/services/productService.js
@@ -52,6 +52,29 @@ exports.getAllProducts = async (
   }
 };
 
+exports.getByTag = async (tag, page = 1, limit = 12) => {
+  try {
+    const skip = (page - 1) * limit;
+    const [products, total] = await Promise.all([
+      Product.find({ tags: { $in: [tag] } })
+        .skip(skip)
+        .limit(limit),
+      Product.countDocuments({ tags: { $in: [tag] } }),
+    ]);
+    return { products, total, page, totalPages: Math.ceil(total / limit) };
+  } catch (error) {
+    throw new Error("Error al obtener los productos por tag");
+  }
+};
+
+exports.getTags = async () => {
+  try {
+    return await Product.distinct("tags");
+  } catch (error) {
+    throw new Error("Error al obtener los tags");
+  }
+};
+
 exports.searchProducts = async (
   name,
   page = 1,
@@ -98,5 +121,37 @@ exports.searchProducts = async (
     return { products, total, page, totalPages: Math.ceil(total / limit) };
   } catch (error) {
     throw new Error("Error al buscar productos");
+  }
+};
+
+exports.getOneProduct = async (id) => {
+  try {
+    return await Product.findById(id);
+  } catch (error) {
+    throw new Error("Error al obtener los productos");
+  }
+};
+
+exports.createProduct = async (productData) => {
+  try {
+    return await Product.create(productData);
+  } catch (error) {
+    throw new Error("Error al crear el producto");
+  }
+};
+
+exports.updateProduct = async (productId, newData) => {
+  try {
+    return await Product.findByIdAndUpdate(productId, newData, { new: true });
+  } catch (error) {
+    throw new Error("Error al actualizar el producto");
+  }
+};
+
+exports.deleteProduct = async (productId) => {
+  try {
+    return await Product.findByIdAndDelete(productId);
+  } catch (error) {
+    throw new Error("Error al eliminar el producto");
   }
 };
